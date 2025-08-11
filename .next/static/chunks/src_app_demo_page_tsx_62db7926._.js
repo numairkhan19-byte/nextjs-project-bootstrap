8c95ed283a8fd5a92c8f002cc724bb348b3fada1
numairkhan19-byte <numair.khan19@gmail.com>
2025-08-11 14:04:14 +0100

    Add .next/static/chunks/src_app_demo_page_tsx_62db7926._.js 1754917454404

---
+++ .next/static/chunks/src_app_demo_page_tsx_62db7926._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_38961737._.js",
+  "static/chunks/node_modules_80752577._.js"
+],
+    source: "dynamic"
+});
